b9c75d6fa0fb17a9960e15549905aac0d91dd0b6
Arbab Khan <arbab.tufail@ocloudsolutions.net>
2023-02-24 20:23:38 +0500

    Crwn-clothing project implementation till Folder09
    In this commit, I have added the Shop page, Created Cart Context, Drop down, and checkout Page and its Functionality.

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import { Routes, Route } from "react-router-dom";
+import Home from "./routes/home/home.component";
+import NavigationBar from "./routes/navigation/navigationBar.component";
+import Authentication from "./routes/authentication/authentication.component";
+import Shop from "./routes/shop/shop.component";
+import CheckOut from "./routes/checkout/checkout.component";
+
+const App = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<NavigationBar />}>
+        <Route index element={<Home />} />
+        <Route path="shop" element={<Shop />} />
+        <Route path="auth" element={<Authentication />} />
+        <Route path="checkout" element={<CheckOut />}></Route>
+      </Route>
+    </Routes>
+  );
+};
+
+export default App;
---
+++ src/component/product-card/product-card.component.jsx
@@ -0,0 +1,30 @@
+import "./product-card.style.scss";
+import Button from "../button/button.component";
+import { useContext } from "react";
+import { CartContext } from "../../context/cart.context";
+
+const ProductCard = (props) => {
+  const { product } = props;
+  const { name, imageUrl, price } = product;
+  const { addItemToCart } = useContext(CartContext);
+
+  const addProductToCart = () => {
+    console.log("hehrere");
+    return addItemToCart(product);
+  };
+
+  return (
+    <div className="product-card-container">
+      <img src={imageUrl} alt={`${name}`} />
+      <div className="footer">
+        <span className="name">{`${name}`}</span>
+        <span className="price">{`${price}`}</span>
+      </div>
+      <Button buttonType="inverted" onClick={addProductToCart}>
+        Add to card
+      </Button>
+    </div>
+  );
+};
+
+export default ProductCard;
