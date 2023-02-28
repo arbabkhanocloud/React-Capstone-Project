e2b37b660d7ba2da31c10f3f77b9a7faaf2bafcc
Arbab Khan <arbab.tufail@ocloudsolutions.net>
2023-02-28 11:21:41 +0500

    crwn-clothing till Folder-10-11
    In this commit, first I stored the Shop data into firebase database. Then I fetched the shop data from firebase. Also added React Styled components in some  style files and also used CSS in JS.

---
+++ src/component/cart-icon/cart-icon.component.jsx
@@ -0,0 +1,19 @@
+import {
+  ShoppingIcon,
+  CartIconContainer,
+  ItemCount,
+} from "./cart-icon.styles.jsx";
+import { useContext } from "react";
+import { CartContext } from "../../context/cart.context";
+
+const CartIcon = () => {
+  const { isCartOpen, setIsCartOpen, cartCount } = useContext(CartContext);
+  const toggleIsCartOpen = () => setIsCartOpen(!isCartOpen);
+  return (
+    <CartIconContainer onClick={toggleIsCartOpen}>
+      <ShoppingIcon className="shopping-icon" />
+      <ItemCount>{`${cartCount}`}</ItemCount>
+    </CartIconContainer>
+  );
+};
+export default CartIcon;
